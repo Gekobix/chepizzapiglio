992d5f22a5606360ca6ddfcec0470620d704b996
Gekobix <costa.jacopo@gmail.com>
2022-05-05 12:07:21 +0200

    You spin me

--- app.js
+++ app.js
@@ -1,50 +1,44 @@
 // Imports
 const express = require("express");
-const { path } = require("express/lib/application");
-const res = require("express/lib/response");
 const sqlite3 = require("sqlite3");
 
-const app = express()
-app.set("view engine", "ejs")
+const app = express();
+app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 
 // Porta
-const port = 3000
+const port = 3000;
 
 // Ascolta sulla porta
-app.listen(port, console.info(`Ascolto su porta ${port}`))
+app.listen(port, console.info(`Ascolto su porta ${port}`));
 
 // Connessione al DB
 const db = new sqlite3.Database("database.db", (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("Connesso al DB Pizze");
-  });
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connesso al DB Pizze");
+});
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
 app.get("/pizzom", (req, res) => {
-    db.get("SELECT * FROM pizze ORDER BY RANDOM() LIMIT 1", (err, row) => {
-        if(err){
-            res.send(err.message)
-        }
-        res.json(row)
-    })
-})
+  db.get("SELECT * FROM pizze ORDER BY RANDOM() LIMIT 1", (err, row) => {
+    if (err) {
+      res.send(err.message);
+    }
+    res.json(row);
+  });
+});
 
-app.get("/:id", (req,res) => {
-    res.sendFile(__dirname + "/public/images/" + req.params.id + ".jpg")
-})
-
-app.get("/tutte", (req, res) => {
-    db.all("SELECT * FROM PIZZE", (err, rows) => {
-        if(err){
-            res.send(err.message)
-        }
-        res.json(rows)
-    })
-})
+app.get("/pizze", (req, res) => {
+  db.all("SELECT * FROM PIZZE", (err, rows) => {
+    if (err) {
+      res.send(err.message);
+    }
+    res.json(rows);
+  });
+});
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,31 @@
 function getPizzom() {
-    fetch("/pizzom")
-    .then(response => response.json())
-    .then(pizza => {
-        document.getElementById("test").innerHTML = 
-        "<p>" + pizza.Nome + " " + pizza.Ingredienti + "</p>" +
-        "<img src='/" + pizza.id + "'></img>"
+  document.getElementById("logo").style.display = "none";
+  fetch("/pizzom")
+    .then((response) => response.json())
+    .then((pizza) => {
+
+      //Divido gli ingredienti per la lista
+      const ingr = pizza.Ingredienti.split(",");
+      let list = "";
+      ingr.forEach(element => {
+        list += "<li>" + element + "</li>"
+      });
+
+      //Rimpiazzo l'HTML con la pizza
+      document.getElementById("name").innerHTML =
+        "<h1> Pigliati una bella " +
+        pizza.Nome +
+        "</h1>";
+      document.getElementById("picture").innerHTML =
+        "<img src='/images/" +
+        pizza.id +
+        ".jpg'></img>";
+      document.getElementById("ingredients").innerHTML =
+        "<p>Ingredienti: </p><ul>" +
+        list + 
+        "</ul>";
     })
+    .catch((error) => {
+      console.log(error);
+    });
 }
